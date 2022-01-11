ee75ea6101b6a10144475237018609fe4ecb91e7
wickedev <orange881217@gmail.com>
2022-01-11 23:16:55 +0900

    @ConstructorBinding on JwtProperties

--- spring-webflux-security-jwt/src/main/kotlin/io/github/wickedev/spring/security/JwtProperties.kt
+++ spring-webflux-security-jwt/src/main/kotlin/io/github/wickedev/spring/security/JwtProperties.kt
@@ -1,13 +1,15 @@
 package io.github.wickedev.spring.security
 
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
 import java.time.Duration
 
+@ConstructorBinding
 @ConfigurationProperties("security.jwt")
 class JwtProperties(
-    var algorithm: String = "EC",
-    var privateKeyPath: String? = "keys/ec256-private.pem",
-    var publicKeyPath: String? = "keys/ec256-public.pem",
-    var accessTokenExpiresIn: Duration? = Duration.ofHours(10),
-    var refreshTokenExpiresIn: Duration? = Duration.ofDays(600),
+    val algorithm: String = "EC",
+    val privateKeyPath: String? = "keys/ec256-private.pem",
+    val publicKeyPath: String? = "keys/ec256-public.pem",
+    val accessTokenExpiresIn: Duration? = Duration.ofHours(10),
+    val refreshTokenExpiresIn: Duration? = Duration.ofDays(600),
 )
